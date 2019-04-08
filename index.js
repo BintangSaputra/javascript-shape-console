86c056b9f3d935efd133dfb12d19a70e31fe30e6
BintangSaputra <bintangcbs26@gmail.com>
2019-04-08 16:24:08 +0700

    add files

---
+++ index.js
@@ -0,0 +1,6 @@
+const rectangleWidth = 200;
+const rectangleLength = 600;
+
+const rectangleArea = rectangleWidth + rectangleLength;
+
+console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength}`);
